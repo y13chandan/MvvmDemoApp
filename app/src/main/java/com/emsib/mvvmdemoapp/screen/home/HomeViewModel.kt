ec5cce744c6ecd8f5b75e77ee0e0bd84d4e4cc0b
Chandan Kumar Singh <chandan.singh@industrybuying.com>
2019-09-15 17:46:34 +0530

    added the view model

--- app/src/main/java/com/emsib/mvvmdemoapp/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/emsib/mvvmdemoapp/screen/home/HomeViewModel.kt
@@ -1,10 +1,34 @@
 package com.emsib.mvvmdemoapp.screen.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.emsib.mvvmdemoapp.models.Post
 import com.emsib.mvvmdemoapp.repository.HomeRepository
+import com.emsib.mvvmdemoapp.util.Coroutines
 
 class HomeViewModel(
     private val repository: HomeRepository
 ): ViewModel() {
+    val showProgress = MutableLiveData<Boolean>()
+    val posts = MutableLiveData<List<Post>>()
 
+    fun getPosts() {
+        showProgress.value = true
+        Coroutines.main {
+            try {
+                posts.value = repository.getPosts()
+                showProgress.value = false
+            } catch (e: Exception) {
+                showProgress.value = false
+            }
+        }
+    }
+
+
+    // if you want to show some specific thing on internet error or on api error then simply use the catch block like this
+//            catch (e: ApiException) {
+//
+//            } catch (e: NoInternetException) {
+//
+//            }
 }
